a17df12cc9f961f32802ae1bae69a6563cb84fcc
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-09-02 07:49:18 +0300

    second chance

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+const titleCased = () =>
+{
+ return tutorials.map(element => (element.split(' ').map(element => element.charAt(0).toUpperCase() + element.slice(1))).join(" "))
 }
